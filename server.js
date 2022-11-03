026229cd9a44a399ed35b56d7d46ea6d4260fddb
mehedi494 <ehasanmunna21@gmail.com>
2022-11-03 08:46:57 +0600

    add app.js to index.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require('dotenv').config();
 const colors = require("colors");
 
-const app = require("./app");
+const app = require(".");
 
 // database connection
 mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
